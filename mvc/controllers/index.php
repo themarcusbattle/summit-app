08e415e437626ae1370e9303cd924995b0f50a03
Marcus Battle <me@marcusbattle.com>
2012-08-26 02:34:11 -0400

    removed link from the index controller

--- mvc/controllers/index.php
+++ mvc/controllers/index.php
@@ -2,7 +2,8 @@
 class indexController extends baseController {
 	
 	public function index() {
-		$this->registry->template->show('checkin');
+		//$this->registry->template->show('checkin');
+		echo "Campus Unity Network for The Summit College Ministry of New Jerusalem Cathedral";
 	}
 	
 	public function setSchool() {
